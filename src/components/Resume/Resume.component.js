d7674d5ecbd1ceafec030be98b51b6b6ec8c6411
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-10-13 00:19:51 +0200

    U

--- src/components/Resume/Resume.component.js
+++ src/components/Resume/Resume.component.js
@@ -18,15 +18,15 @@ export const Resume = () => {
             <span>•</span>Education
           </h1>
           <p>
-          <ul>
-            <li> Udemy, Master JavaScript | Jun 2021 - Present</li>
-            <li>
-              HackYourFuture, Full Stack Development | Nov 2021 - Aug 2022
-            </li>
-            <li>
-              Iraqi School of Sofia, BG, High School | Sep 2015 - Jun 2018
-            </li>
-          </ul>
+            <ul>
+              <li> Udemy, Master JavaScript | Jun 2021 - Present</li>
+              <li>
+                HackYourFuture, Full Stack Development | Nov 2021 - Aug 2022
+              </li>
+              <li>
+                Iraqi School of Sofia, BG, High School | Sep 2015 - Jun 2018
+              </li>
+            </ul>
           </p>
         </div>
 
@@ -35,15 +35,15 @@ export const Resume = () => {
             <span>•</span>Experience
           </h1>
           <p>
-          <ul>
-            <li>
-              Freelance platforms exert Social media | Jan 2016 - Present
-            </li>
-            <li>
-              Mobile Store, Computer & Phone Repairman | Feb 2016 - Nov 2017
-            </li>
+            <ul>
+              <li>
+                Freelance platforms exert Social media | Jan 2016 - Present
+              </li>
+              <li>
+                Mobile Store, Computer & Phone Repairman | Feb 2016 - Nov 2017
+              </li>
             </ul>
-            </p>
+          </p>
         </div>
 
         <div>
@@ -51,18 +51,18 @@ export const Resume = () => {
             <span>•</span>Skills
           </h1>
           <p>
-          <ul>
-            <li>HTML5</li>
-            <li>Cascading Style Sheets (CSS)</li>
-            <li>JavaScript</li>
-            <li>MySQL</li>
-            <li>Reactis</li>
-            <li>Nodejs</li>
-            <li>Knexis</li>
-            <li>Expressjs</li>
-            <li>Git</li>
+            <ul>
+              <li>HTML5</li>
+              <li>Cascading Style Sheets (CSS)</li>
+              <li>JavaScript</li>
+              <li>MySQL</li>
+              <li>Reactis</li>
+              <li>Nodejs</li>
+              <li>Knexis</li>
+              <li>Expressjs</li>
+              <li>Git</li>
             </ul>
-            </p>
+          </p>
         </div>
 
         <div>
@@ -73,24 +73,24 @@ export const Resume = () => {
             <span>•</span>Languages
           </p>
           <p>
-          <ul className="OtherUl">
-            <li>Arabic</li>
-            <li>Danish</li>
-            <li>English</li>
+            <ul className="OtherUl">
+              <li>Arabic</li>
+              <li>Danish</li>
+              <li>English</li>
             </ul>
-            </p>
+          </p>
           <p className="Other">
             <span>•</span>Hobbies
           </p>
           <p>
-          <ul className="OtherUl">
-            <li>Billiards</li>
-            <li>Coding</li>
-            <li>Drawing</li>
-            <li>Table tennis</li>
-            <li>Volleyball</li>
+            <ul className="OtherUl">
+              <li>Billiards</li>
+              <li>Coding</li>
+              <li>Drawing</li>
+              <li>Table tennis</li>
+              <li>Volleyball</li>
             </ul>
-            </p>
+          </p>
         </div>
       </div>
     </div>
