901bd47b711508c3add527dd03b7fec652a1bc86
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-10-13 19:05:51 +0200

    U

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFacebookSquare, faInstagramSquare ,faTwitterSquare, faLinkedin, faGithubSquare,} from "@fortawesome/free-brands-svg-icons";
+import {faFacebook, faInstagram ,faTwitter, faLinkedinIn, faGithub,} from "@fortawesome/free-brands-svg-icons";
 export const Footer = () => {
   return (
       <div className="footer">
@@ -9,11 +9,11 @@ export const Footer = () => {
           <div className="hk">☺ 2022 by Hussein Khaleefah. </div>
           
            <div className="social-media-icon-container">
-             <div> <a href="https://www.facebook.com/hassou2n/" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faFacebookSquare} /></a></div>
-             <div> <a href="https://www.instagram.com/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faInstagramSquare} /></a></div>
-             <div> <a href="https://twitter.com/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faTwitterSquare} /></a></div>
-             <div> <a href="https://www.linkedin.com/in/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faLinkedin} /></a></div>
-             <div> <a href="https://github.com/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faGithubSquare} /></a></div>
+             <div> <a href="https://www.facebook.com/hassou2n/" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faFacebook} /></a></div>
+             <div> <a href="https://www.instagram.com/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faInstagram} /></a></div>
+             <div> <a href="https://twitter.com/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faTwitter} /></a></div>
+             <div> <a href="https://www.linkedin.com/in/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faLinkedinIn} /></a></div>
+             <div> <a href="https://github.com/hassou2n/" target="_blank" rel="noopener noreferrer">  <FontAwesomeIcon icon={faGithub} /></a></div>
            </div>
         </div>
       </div>
