fdd301fff352086c663af361bbc899d637851ced
Hussein Khaleefah <husseinosamah2@gmail.com>
2022-12-17 00:30:36 +0100

    U

--- src/components/AboutMe/AboutMe.component.js
+++ src/components/AboutMe/AboutMe.component.js
@@ -2,17 +2,73 @@
 
 import React from "react";
 import "./AboutMe.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLinkedinIn,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const AboutMe = () => {
   return (
     <section id="aboutMe">
       <div className="AboutMe">
+        <div className="social-media-icon-containerAboutMe">
+          <div class="vl"></div>
+          <div>
+            <a
+              href="https://www.facebook.com/hassou2n/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.instagram.com/hassou2n/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://twitter.com/hassou2n/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/hassou2n/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/hassou2n/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
+        </div>
         <div class="container text-center">
           <div class="row">
             <div class="col-lg-8 col-sm-8">
               <div class="circle-image">
                 <img
-                  src="assets/images/my-img.jpg"
+                  src="assets/images/my-img.png"
                   width="50px"
                   height="50px"
                   alt="img"
