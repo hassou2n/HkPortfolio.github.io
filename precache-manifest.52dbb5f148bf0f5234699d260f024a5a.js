4933768ef58f4bd4e86cb2bbbaa8fecb470b9cc8
hassou2n <husseinosamah2@gmaiil.com>
2023-04-16 02:02:05 +0200

    Updates

---
+++ precache-manifest.52dbb5f148bf0f5234699d260f024a5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44b15f4fab5fc244f76",
+    "url": "/static/css/main.bfcd7ab3.chunk.css"
+  },
+  {
+    "revision": "b44b15f4fab5fc244f76",
+    "url": "/static/js/main.b44b15f4.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "71e4e6630332641d006b",
+    "url": "/static/js/2.71e4e663.chunk.js"
+  },
+  {
+    "revision": "9533ad4d11ae861fee58",
+    "url": "/static/js/runtime~main.9533ad4d.js"
+  },
+  {
+    "revision": "5b6d5ba8772282ed4ca0aa6839a459f4",
+    "url": "/static/media/icomoon.5b6d5ba8.eot"
+  },
+  {
+    "revision": "2b47cb1e807c10be1fbf49b51c560913",
+    "url": "/static/media/icomoon.2b47cb1e.ttf"
+  },
+  {
+    "revision": "9010f12282e711185ee902515b37c386",
+    "url": "/static/media/icomoon.9010f122.woff"
+  },
+  {
+    "revision": "3561baa4069b820babfecf6462e57ef3",
+    "url": "/static/media/icomoon.3561baa4.svg"
+  },
+  {
+    "revision": "93405e38e6d264c73372f6f301e02a02",
+    "url": "/index.html"
+  }
+];
